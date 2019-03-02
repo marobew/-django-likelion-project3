ac8abc7c3d0fafd2301a64c2de3d203c652d2415
marobew <boramm@icloud.com>
2019-03-02 15:28:01 +0900

    commit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get(
 SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
